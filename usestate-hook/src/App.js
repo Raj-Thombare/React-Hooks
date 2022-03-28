c099351fe1bf9ba461f30f2fca93aa1b06f45b56
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2022-03-28 18:14:41 +0530

    feat: create random color generator using useState

--- usestate-hook/src/App.js
+++ usestate-hook/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState('#2016A6');
+
+  const changeColor = () => {
+    setColor(randomColorGenerator)
+  }
+
+  const randomColorGenerator = () => {
+    return `#${Math.random().toString(16).substring(2,8)}`
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Color Generator</h1>
+      <div className="container">
+        <div className="box" style={{backgroundColor:color}}>
+          <div className='colorcode'>
+          {color}
+          </div>
+        </div>
+        <button onClick={changeColor} style={{backgroundColor:color}}>Change Color</button>
+      </div>
     </div>
   );
 }
